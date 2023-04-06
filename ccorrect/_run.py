9830a84ddbe6be60300537488598c59863a5c378
mpostaire <maxime_postaire@hotmail.fr>
2023-04-06 23:39:42 +0200

    run: raise RuntimeError if gdb exits with return code != 0 (fixes ci)

--- ccorrect/_run.py
+++ ccorrect/_run.py
@@ -8,7 +8,7 @@ def run(test_script, silent_gdb=True):
     cmd = _get_cmd(test_script, silent_gdb)
     p = subprocess.run(shlex.split(cmd))
     if p.returncode != 0:
-        return None
+        raise RuntimeError(f"GDB exited with return code: {p.returncode}")
 
     try:
         with open(os.path.join(os.path.dirname(test_script), "results.yml"), "r") as f:
