e56bbbd2f4f9309663a8eb410baa1ffe176bcf44
mpostaire <maxime_postaire@hotmail.fr>
2023-07-30 11:04:25 +0200

    fix igninious example pushed "q1" tag when it shouldn't
    the "q1" tag could be pushed when the previous tests failed with an addresssanitizer error. This is fixed by assuming that tests are failed before running them.

--- example/inginious_task_calloc2/student/test.py
+++ example/inginious_task_calloc2/student/test.py
@@ -2,7 +2,7 @@ import ccorrect
 import gdb
 
 
-flag = True
+succeeded = 0
 
 class TestStudentCode(ccorrect.TestCase):
     debugger = ccorrect.Debugger("test")
@@ -12,64 +12,59 @@ class TestStudentCode(ccorrect.TestCase):
         description="Allocated memory : test calloc2"
     )
     def test_calloc2_1(self):
-        global flag
+        global succeeded
         calloc2 = self.debugger.function("calloc2")
 
         with self.debugger.watch("malloc"):
             ret = calloc2(16, 4)
-            if ret == 0:
-                flag = False
+            if ret == 0 or not self.debugger.malloced(ret):
                 self.fail("Erreur lors l'allocation de la mémoire.")
 
         if self.debugger.allocated_size() != 16 * 4:
-            flag = False
             self.push_tag("not_malloc_once")
             self.fail("You allocated more memory than required.")
 
-        if ret == 0 or not self.debugger.malloced(ret):
-            flag = False
-
         if self.debugger.stats["malloc"].called != 1:
-            flag = False
             self.push_tag("not_malloc_once")
             self.fail("Why didn't you call malloc exactly once?")
+
+        succeeded += 1
 
     @ccorrect.test_metadata(
         problem="calloc2",
         description="Initialised memory : test calloc2"
     )
     def test_calloc2_2(self):
-        global flag
+        global succeeded
         calloc2 = self.debugger.function("calloc2")
 
         ptr = calloc2(42, 1)
         if ptr == 0:
-            flag = False
             self.fail("Erreur lors l'allocation de la mémoire.")
 
         if ptr != 0:
             ptr = ptr.cast(gdb.lookup_type("char").pointer())
             if (ptr + 10).dereference() != 0:
-                flag = False
                 self.fail("You didn't initialise the allocated memory.")
+
+        succeeded += 1
 
     @ccorrect.test_metadata(
         problem="calloc2",
         description="Fail of malloc : test calloc2"
     )
     def test_calloc2_3(self):
-        global flag
+        global succeeded
         calloc2 = self.debugger.function("calloc2")
 
         with self.debugger.fail("malloc", retval=ccorrect.Ptr(0)):
             ptr = calloc2(42, 42)
 
         if ptr != 0:
-            flag = False
             self.push_tag("malloc_fail_handling")
             self.fail("Don't forget that malloc can fail sometimes.")
 
-        if flag:
+        if succeeded == 2:
             self.push_tag("q1")
 
 
